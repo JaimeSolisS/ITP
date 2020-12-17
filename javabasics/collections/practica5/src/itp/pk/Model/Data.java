8757ab0edc9a16516a7212f403a59e43de3cad8c
JaimeSolisS <jaimesolissoto@me.com>
2020-12-17 16:49:30 -0700

    Feat Search and Remove

---
+++ javabasics/collections/practica5/src/itp/pk/Model/Data.java
@@ -0,0 +1,33 @@
+package itp.pk.Model;
+
+public class Data implements Comparable<Data> {
+    public Long id;
+    public String name;
+    public String key;
+    public String status;
+
+
+    public Data(Long id, String name, String key, String status) {
+        this.id = id;
+        this.name=name;
+        this.key=key;
+        this.status = status;
+    }
+
+    public int compareTo(Data e){ //implementing abstract method.
+        if(id>e.id){
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", key='" + key + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
