d7c70455f16bafaa97b4e54efd2950def44a3ddf
JaimeSolisS <jaimesolissoto@me.com>
2021-03-30 18:44:30 -0700

    Feat read request payload body

---  Big Data/bootcamp/python/Rest/practice9/rest.py
+++  Big Data/bootcamp/python/Rest/practice9/rest.py
@@ -2,21 +2,14 @@ from app import app
 from db import mysql
 from flask import jsonify
 import logging
-from flask import request
+from flask import request, g
 import secrets
 
 logging.basicConfig(level=logging.DEBUG)
 # PRETTY
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-
-# id
-# name
-# key_value
-# status
-
 token = 'bearer ' + secrets.token_urlsafe(16)
 print(token)
-
 
 @app.before_request
 def before_request_func():
@@ -24,23 +17,27 @@ def before_request_func():
     if request.headers.get('Auth'):
         reqtoken = request.headers.get('Auth')
         if reqtoken == token:
-            pass
+             # ------Open Connection----
+            g.connection = mysql.connect()
+            g.cursor = g.connection.cursor()
         else:
-            response = jsonify('Wrong token')
+            response = jsonify('Invalid token')
             response.status_code = 401
             return response
     else:
-        response = jsonify('No token found')
+        response = jsonify('No Authorization header was found')
         response.status_code = 401
         return response
 
+# @app.after_request
+# def after_request_func():
+#    g.cursor.close()
+#    g.connection.close()
 
 @app.route('/users', methods=['GET'])
 def users():
     try:
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM users")
+        g.cursor.execute("SELECT * FROM users")
         records = cursor.fetchall()
         # Response
         response = jsonify(records)
@@ -51,18 +48,13 @@ def users():
         print(e)
 
     finally:
-        cursor.close()
-        connection.close()
-
+        g.cursor.close()
+        g.connection.close()
 
 @app.route('/users/<int:id>')
 def user(id):
-    connection = None
-    cursor = None
     try:
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        cursor.execute(
+        g.cursor.execute(
             "SELECT * FROM users WHERE id=%s", id)
         row = cursor.fetchone()
         response = jsonify(row)
@@ -71,78 +63,68 @@ def user(id):
     except Exception as e:
         print(e)
     finally:
-        cursor.close()
-        connection.close()
+        g.cursor.close()
+        g.connection.close()
 
 
 @app.route('/delete/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    connection = mysql.connect()
-    cursor = connection.cursor()
     try:
-        cursor.execute("DELETE FROM users WHERE id=%s", (id,))
-        connection.commit()
+        g.cursor.execute("DELETE FROM users WHERE id=%s", (id,))
+        g.connection.commit()
         response = jsonify('User deleted successfully!')
         response.status_code = 200
         return response
     except Exception as e:
         print(e)
     finally:
-        cursor.close()
-        connection.close()
+        g.cursor.close()
+        g.connection.close()
 
 
 @app.route('/add', methods=['POST'])
 def add_user():
-    connection = mysql.connect()
-    cursor = connection.cursor()
     try:
         # ----REQUEST-------
-        id = request.headers.get('id')
-        name = request.headers.get('name')
-        key_value = request.headers.get('key_value')
-        status = request.headers.get('status')
+        _json = request.json
+        _id = _json['id']
+        _name = _json['name']
+        _key_value = _json['key_value']
+        _status = _json['status']
 
         # ----INSERT INTO MSQL------
         sql = "INSERT INTO users(id, name, key_value, status) VALUES(%s, %s, %s, %s)"
         data = (id, name, key_value, status)
-        cursor.execute(sql, data)
-        connection.commit()
+        g.cursor.execute(sql, data)
+        g.connection.commit()
 
         # -----RESPONSE-----------
         response = jsonify('User added successfully!')
         response.status_code = 200
-        response.headers['id'] = id
-        response.headers['name'] = name
-        response.headers['key_value'] = key_value
-        response.headers['status'] = status
-        print(response)
+
         return response
     except Exception as e:
         print(e)
     finally:
-        cursor.close()
-        connection.close()
+        g.cursor.close()
+        g.connection.close()
 
 
 @app.route('/update', methods=['PUT'])
 def update_user():
-    connection = mysql.connect()
-    cursor = connection.cursor()
     try:
         # ----REQUEST-------
-        id = request.headers.get('id')
-        name = request.headers.get('name')
-        key_value = request.headers.get('key_value')
-        status = request.headers.get('status')
+        _json = request.json
+        _id = _json['id']
+        _name = _json['name']
+        _key_value = _json['key_value']
+        _status = _json['status']
 
         # ----UPDATE------
         sql = "UPDATE users SET name=%s, key_value=%s, status=%s WHERE id=%s"
         data = (name, key_value, status, id)
-        connection = mysql.connect()
-        cursor = connection.cursor()
-        cursor.execute(sql, data)
-        connection.commit()
+        g.cursor.execute(sql, data)
+        g.connection.commit()
 
         # --- RESPONSE ----
         response = jsonify('User updated successfully!')
@@ -151,9 +133,8 @@ def update_user():
     except Exception as e:
         print(e)
     finally:
-        cursor.close()
-        connection.close()
-
+        g.cursor.close()
+        g.connection.close()
 
 if __name__ == "__main__":
     app.run()
